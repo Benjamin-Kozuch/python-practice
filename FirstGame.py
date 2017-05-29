80d997043b772f002fcd6d9612a257288a0e2fb9
Benjamin-Kozuch <Benjamin.kozuch@nyu.edu>
2017-05-29 17:58:18 -0400

    move box

--- FirstGame.py
+++ FirstGame.py
@@ -6,7 +6,8 @@ screen = pygame.display.set_mode((1000, 300))  # w,h
 
 done = False
 is_blue = True
-
+x = 30
+y = 30
 
 
 while not done:
@@ -16,9 +17,15 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]: y -= 3
+    if pressed[pygame.K_DOWN]: y += 3
+    if pressed[pygame.K_LEFT]: x -= 3
+    if pressed[pygame.K_RIGHT]: x += 3
+
     if is_blue: color = (0, 128, 255)
     else: color = (255, 100, 0)
-    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
 
     pygame.display.flip()
 
